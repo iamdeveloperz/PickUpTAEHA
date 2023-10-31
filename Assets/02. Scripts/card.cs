1d8f27033f9aac148f6f1ad26476c098652f4887
simderella <146172523+simderella@users.noreply.github.com>
2023-10-31 16:52:53 +0900

    20231031심태용애니메이션

---
+++ Assets/02. Scripts/card.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class card : MonoBehaviour
+{
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void openCard()
+    {
+        anim.SetBool("isOpen", true);
+        transform.Find("Front").gameObject.SetActive(true);
+        transform.Find("Back").gameObject.SetActive(false);
+    }
+}
