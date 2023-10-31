b5f065779691d5e6506283e386e5d3a6a6d9d25e
ElwINLuyA <149444150+ElwINLuyA@users.noreply.github.com>
2023-10-31 19:00:01 +0900

    2023-10-31 승준
    프리팹 설정 및 게임 매니저 시간 설정

---
+++ Assets/02. Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public TMP_Text timeTxt;
+    float time = 30.00f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+        timeTxt.text = time.ToString("N2");
+    }
+}
