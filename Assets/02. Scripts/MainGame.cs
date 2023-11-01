4dfb918fbacd3afcd81092d02ca81f6873ae2fbd
Ryutaeha <taeha61@gmail.com>
2023-11-01 21:21:45 +0900

    2023-11-01 이미지 버그 수정

--- Assets/02. Scripts/MainGame.cs
+++ Assets/02. Scripts/MainGame.cs
@@ -98,7 +98,17 @@ public class MainGame : MonoBehaviour
 
             card.transform.position = new Vector3(x, y, 0);
 
-            string cardName = "CardImage" + CardImages[i].ToString();
+            string cardName = null;
+            if (i < 18)
+            {
+                //카드 숫자가 1자리일때
+                cardName = "CardImage0"+ CardImages[i].ToString();
+            }
+            else
+            {
+                //2자리 일때
+                cardName = "CardImage" + CardImages[i].ToString();
+            }
             card.transform.name = cardName;
             card.transform.Find("Front").GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(cardName);
         }
